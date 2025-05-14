6d3e8ef99211aee0f441dd503c262022d20d5b9d
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-05-13 22:01:53 -0400

    user?

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,14 +5,14 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        firstname='First',lastname='Last',username='Demo', email='demo@aa.io', phone=2692211000, password='password')
-    # marnie = User(
-        # username='marnie', email='marnie@aa.io', password='password')
+        firstname='First',lastname='Last', username='Demo', email='demo@aa.io', phone="(269)221-1600", password='password')
+    marnie = User(
+         firstname='First',lastname='Last', username='marnie', email='marnie@aa.io', phone="(269)221-1500", password='password')
     bobbie = User(
-         firstname='First',lastname='Last',username='bobbie', email='bobbie@aa.io', phone=2692211000, password='password')
+         firstname='First',lastname='Last', username='bobbie', email='bobbie@aa.io', phone="(269)221-1770", password='password')
 
     db.session.add(demo)
-    # db.session.add(marnie)
+    db.session.add(marnie)
     db.session.add(bobbie)
     db.session.commit()
 
